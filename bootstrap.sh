398b7db6a3b12e840233f9f795ea5bb35d0ace38
sawanoboly <sawanoboriyu@higanworks.com>
2014-07-10 23:17:12 +0900

    pemission probrems for packer

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -ex
+
 JETTY_VERSION='9.2.1.v20140609'
 GB_VERSION='2.1'
 DEFAULT_USER='ec2-user'
@@ -23,18 +25,19 @@ sudo install -o ${JETTY_USER} -g ${JETTY_GROUP} -d /opt/gitbucket
 wget ftp://ftp.jaist.ac.jp/pub/eclipse/jetty/stable-9/dist/jetty-distribution-${JETTY_VERSION}.tar.gz -O /opt/src/jetty.tar.gz
 
 ## Extract jetty
-tar xvzf /opt/src/jetty.tar.gz -C /opt/gitbucket/
+sudo tar xvzf /opt/src/jetty.tar.gz -C /opt/gitbucket/
 sudo mv /opt/gitbucket/jetty-distribution-${JETTY_VERSION} /opt/gitbucket/jetty
-sudo install -o ${JETTY_USER} -g ${JETTY_GROUP} -d /opt/gitbucket/jetty/webapps/ROOT
 
-wget https://github.com/takezoe/gitbucket/releases/download/${GB_VERSION}/gitbucket.war -O /opt/src/gitbucket.war
-sudo cp /opt/src/gitbucket.war /opt/gitbucket/jetty/webapps/ROOT/
 
 ## Copy initial script
 sudo install -o root -g root -m 0755 /opt/gitbucket/jetty/bin/jetty.sh /etc/init.d/gitbucket
 
 # Extract gitbucket
-cd /opt/gitbucket/jetty/webapps/ROOT
+mkdir ROOT
+wget https://github.com/takezoe/gitbucket/releases/download/${GB_VERSION}/gitbucket.war -O ROOT/gitbucket.war
+cd ROOT
 sudo jar xvf gitbucket.war
-sudo chown -R ${JETTY_USER}.${JETTY_GROUP} /opt/gitbucket/jetty/webapps/ROOT
 sudo rm -f gitbucket.war
+cd ..
+sudo mv ROOT /opt/gitbucket/jetty/webapps/
+sudo chown -R ${JETTY_USER}.${JETTY_GROUP} /opt/gitbucket/jetty/webapps/ROOT
