e666e13069fdc11264cf723373bc0209247ff0ec
sawanoboly <sawanoboriyu@higanworks.com>
2016-02-22 20:42:53 +0900

    use curl instead of wget

--- bootstrap.sh
+++ bootstrap.sh
@@ -23,7 +23,7 @@ sudo install -o ${JETTY_USER} -g ${JETTY_GROUP} -d /opt/gitbucket
 
 
 ## Download  latest stable jetty to /opt
-wget http://download.eclipse.org/jetty/stable-9/dist/jetty-distribution-${JETTY_VERSION}.tar.gz -O /opt/src/jetty.tar.gz
+curl http://download.eclipse.org/jetty/stable-9/dist/jetty-distribution-${JETTY_VERSION}.tar.gz -o /opt/src/jetty.tar.gz
 
 ## Extract jetty
 sudo tar xvzf /opt/src/jetty.tar.gz -C /opt/gitbucket/
@@ -35,6 +35,6 @@ sudo chown jetty.jetty -R /opt/gitbucket/jetty
 sudo install -o root -g root -m 0755 /opt/gitbucket/jetty/bin/jetty.sh /etc/init.d/gitbucket
 
 # Extract gitbucket
-wget https://github.com/takezoe/gitbucket/releases/download/${GB_VERSION}/gitbucket.war -O root.war
+curl https://github.com/takezoe/gitbucket/releases/download/${GB_VERSION}/gitbucket.war -o root.war
 sudo mv root.war /opt/gitbucket/jetty/webapps/
 sudo chown -R ${JETTY_USER}.${JETTY_GROUP} /opt/gitbucket/jetty/webapps/
